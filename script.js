8e23ed71318438feb00b9b42c934ccc0356c1d00
unknown <ryanali456@gmail.com>
2020-12-09 02:22:45 +0800

    animation and JS fix

--- script.js
+++ script.js
@@ -1,71 +1,81 @@
-const card = document.getElementById("card-container");
-const postWarToPresent = document.querySelectorAll('#Post')
-const postWarToPresentRead = document.querySelector('#Present')
-const postWarToPresentReturn = document.querySelectorAll('#present_return')
+const card = document.querySelector('#card-container');
 const main = document.querySelector('.main');
-
-const COUNTRIES = [
+const COUNTRIES = [ // country objects
           {
                name:"PRE",
-               container: document.getElementById("Pre")
+               container: document.querySelector('#Pre')
           },
           {
                name:"SPAIN",
-               container: document.getElementById("Spa")
+               container: document.querySelector('#Spa')
           },
           {
                name:"AMERICA",
-               container: document.getElementById("Ame")
+               container: document.querySelector('#Ame')
           },
           {
                name:"JAPAN",
-               container: document.getElementById("Jap")
+               container: document.querySelector('#Jap')
           }
+          ,
+          {
+               name:"PRESENT",
+               container: document.querySelectorAll('#Post')
+          }
+     
 ]
-//helper function sleep
-function sleep(ms) {  return new Promise(resolve => setTimeout(resolve, ms));}
-//close function
-function close(container){
+sleep=(ms)=>new Promise(resolve => setTimeout(resolve, ms)); //helper function sleep timeout
+isArrayOfDOM=(container)=>container.length === undefined ? true : false; //helper function  to check if DOM array or not
+
+close = (container)=>{ //close function
   let isAnimationFinished = async()=>{
+     if(isArrayOfDOM(container)){ //if container is not array of DOM
           container.style.animation = "close 2s linear";      
-          await sleep(2000);
-          return true;
+          await sleep(2000);//wait 2 seconds before return promise
+          return false;
      }
-     isAnimationFinished().then(()=>{
-          container.style.display = "none";
-          card.style.display ="flex";
-          main.style.animation = "reloadBody 2s linear";
+          container.forEach((containerElement)=>{ // if container is array of DOM 
+          containerElement.style.animation = "close 2s linear";     
+     }) 
+          await sleep(2000); //wait 2 seconds before return promise
+          return true;
+}    
+isAnimationFinished().then((isArray)=>{
+     card.style.display ="flex";
+     main.style.animation = "reloadBody 2s linear";// animate body
+      if(isArray){
+               // check again if array of DOM
+               container.forEach((containerElements)=>{
+               containerElements.style.display = "none" //hide container elements
+               })
+               return
+     }
+          container.style.display = "none";// if is not array of DOM
      })
 }
-//open function
-function open(container){
+open=(container)=>{ //open function
      card.style.display ="none";
+     if(isArrayOfDOM(container)){//if is not array of DOM
      container.style.display = "flex";
      container.style.animation = "open 2s linear";
+     return 
 }
-//return function
-function returnToMain(name){
+      container.forEach((containerElement)=>{ // if is array of DOM
+          containerElement.style.display = "flex";
+          containerElement.style.animation = "open 2s linear";
+      })
+}
+returnToMain=(name)=>{//return function
      COUNTRIES.forEach((country)=>{
-          if(country.name === name){
+          if(country.name === name){ //check if args passed by onClick is equal to country.name on object of countries
                close(country.container)
-          }
-          if(name === "PRESENT"){
-           postWarToPresent.forEach((containerElement)=>{
-               close(containerElement)
-          }) 
           }
      })
  }
- //read function
- function readArticle(name){
+ readArticle=(name)=>{ //read function
       COUNTRIES.forEach((country)=>{
-           if(country.name === name){
+           if(country.name === name){ //check if args passed by onClick is equal to country.name on object of countries
                 open(country.container)
-           }
-           if(name === 'PRESENT'){
-               postWarToPresent.forEach((containerElement)=>{
-               open(containerElement)
-               }) 
            }
       })
  }
